1c8899f5b1a202e7b5d946f3aa00b8f1ea5c58ed
Mathias <mpenavai@enssat.fr>
2024-03-25 12:29:49 +0100

    implémentation de TouteSol (non terminée)

--- projet/algorythmique_avancee_Penavaire_Peuch/src/essaie_successifs/ToutesSol.java
+++ projet/algorythmique_avancee_Penavaire_Peuch/src/essaie_successifs/ToutesSol.java
@@ -1,6 +1,41 @@
 package essaie_successifs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToutesSol {
 
-    
+
+
+    public static void rendreMonnaie (List<Integer> c, int n, List<Integer> solution){
+        
+        for (int i = 0; i< c.size(); i++) {
+            if (n >= 0 && c.get(i) <= n){ // satisfaisant
+                // enregistrer
+                solution.add(c.get(i));
+                n -= c.get(i);
+
+                if (n == 0) { // soltrouvée
+                    // écriresol
+                    System.out.println(solution);
+                }
+                else {
+                    //toutessol(i+1)
+                    rendreMonnaie(c, n, solution);
+                }
+            }
+            // défaire
+            n += c.get(i);
+            solution.remove(solution.size() - 1);
+        }
+        
+        
+        return;
+        
+    }
+
+    public static void main(String[] args) {
+        List<Integer> c = new ArrayList<>();
+        
+    }
 }
